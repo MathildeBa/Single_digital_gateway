f930893bd5b4eb49fe81cfbfc8570b8197520cae
FredericBinon <fredericbinn@gmail.com>
2019-03-30 15:30:52 +0100

    modifying index.html + adding bases to main.js

---
+++ assets/script/main.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  document.querySelector("#postIt").style.display = "none";
+
+  document.querySelector("#tool").addEventListener("click", ()=>{
+    document.querySelector("#postIt").style.display = "block";
+    // document.querySelector("#tool").style.backgroundImage="";
+    // document.querySelector("#closed").addEventListener("click", ()=>{
+    //   document.querySelector("#postIt").style.display = "none";
+    // });
+  });
+
+}
