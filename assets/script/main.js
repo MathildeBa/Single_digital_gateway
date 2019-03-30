f69102021ab30622ad1551e99a8d11c2cc0ef18a
FredericBinon <fredericbinn@gmail.com>
2019-03-30 22:23:09 +0100

    files modification

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,20 +1,53 @@
 window.onload = () => {
-  // document.querySelector("#postIt").style.display = "none";
+  document.querySelector("#postIt").style.display = "none";
+
+  // const targetDiv = (e)=>{
+  //   let target=e.target;
+  //   let test = target.innerHTML;
+  //   if( test.indexOf('</p>') == -1){
+  //     e.target.style.border="1px solid blue";
+  //   }
+  //     // e.target.style.border="1px solid blue";
+  // }
+  // const untargetDiv = (e)=>{
+  //   let target=e.target;
+  //
+  //   e.target.style.border="none";
+  // }
+  // const clickTargetDiv = (e)=>{
+  //   let target=e.target;
+  //   let test = target.innerHTML;
+  //   if( test.indexOf('</p>') == -1){
+  //     console.log(target.innerText);
+  //   }
+  // }
+
+  document.addEventListener('mouseover',targetDiv);
+  document.addEventListener('mouseout',untargetDiv);
+  document.addEventListener('click',clickTargetDiv);
 
   document.querySelector("#tool").addEventListener("click", ()=>{
     document.querySelector("#postIt").style.display = "block";
-    // document.querySelector("#tool").style.backgroundImage="";
+    document.querySelector("#tool").style.backgroundImage="url('assets/images/tools/btn_while_write.png')";
+
     document.querySelector("#closed").addEventListener("click", ()=>{
       document.querySelector("#postIt").style.display = "none";
+      document.querySelector("#help").style.display = "none";
+      document.querySelector("#problem").style.display = "block";
+      document.querySelector("#postIt").style.backgroundImage="url('assets/images/tools/postIt_yellow.png')";
+      document.querySelector("#tool").style.backgroundImage="url('assets/images/tools/btn_feedback.png')";
     });
-    document.querySelector("#help").style.display = "none";
+
     document.querySelector("#helpTitle").addEventListener("click", ()=>{
       document.querySelector("#problem").style.display = "none";
-      document.querySelector("#help").style.display = "block";
+      document.querySelector("#help").style.display = "flex";
+      document.querySelector("#postIt").style.backgroundImage="url('assets/images/tools/postIt_blue.png')";
     });
+
     document.querySelector("#problemTitle").addEventListener("click", ()=>{
       document.querySelector("#help").style.display = "none";
       document.querySelector("#problem").style.display = "block";
+      document.querySelector("#postIt").style.backgroundImage="url('assets/images/tools/postIt_yellow.png')";
     });
   });
 
